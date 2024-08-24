fed2c80109d5527faad002ff9c072883071b8df5
Jan Justi <9977169+janjusti@users.noreply.github.com>
2024-08-24 19:23:52 -0300

    inserção de favicon

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,13 @@ module.exports = {
         test: /\.(css|sass|scss)$/,
         use: ['css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(ico|png|jp?g|webp|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][ext]',
+        },
+      },
     ],
   },
 };
